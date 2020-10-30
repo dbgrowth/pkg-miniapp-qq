71ede17ab929ca2cb5c48273d202c45bf98c14c5
wuchongde <wuchongde@uniontech.com>
2020-10-30 12:00:03 +0800

    first commit

---
+++ weapp.go
@@ -0,0 +1,12 @@
+package weapp
+
+const (
+	// baseURL qq请求基础URL
+	baseURL = "https://api.q.qq.com"
+)
+
+// POST 参数
+type requestParams map[string]interface{}
+
+// URL 参数
+type requestQueries map[string]string
